4c246c4f7a45b8fd33b3b1e205583940dbb9545b
zhaolin_flow <zhaolin_flow@qq.com>
2018-10-07 15:03:31 +0800

    eclipse第一次提交

---
+++ src/main/java/com/kgc/pojo/Apple.java
@@ -0,0 +1,18 @@
+package com.kgc.pojo;
+
+public class Apple {
+   private int age;
+   private double price;
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+}
